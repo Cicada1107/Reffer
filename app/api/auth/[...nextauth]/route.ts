70e6b3ea7014498940953a3ff2e9a0026bc0bc4f
Cicada1107 <arijitdubey2018@gmail.com>
2025-06-28 14:12:54 +0530

    Added prisma and database schema

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,33 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { prisma } from "@/lib/prisma"
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async jwt({ token, user }){
-        if(user){
-            token.id = user.id;
-        }
-        return token;
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        session.user.id = token.sub!
+      }
+      return session
     },
-    async session({ session, token }){
-        if(token){
-            session.user.id = token.id as string;
-        }
-        return session;
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.uid = user.id
+      }
+      return token
     },
   },
-});
+  session: {
+    strategy: 'jwt',
+  },
+})
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
