fc50e2c26ec1d84f3ccc7e02adf99331a42f878e
Cicada1107 <arijitdubey2018@gmail.com>
2025-07-04 12:50:36 +0530

    socketio.ts - fixed the url

--- pages/api/socketio.ts
+++ pages/api/socketio.ts
@@ -15,7 +15,7 @@ export default async function ioHandler(
     const io = new ServerIO(httpServer, {
       path: '/api/socketio',
       cors: {
-        origin: process.env.NEXTAUTH_URL || "http://localhost:3000" || "http://reffer-sigma.vercel.app",
+        origin: process.env.NEXTAUTH_URL || "http://localhost:3000" || "https://reffer-sigma.vercel.app/",
         methods: ["GET", "POST"]
       }
     });
